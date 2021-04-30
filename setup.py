1fdc7c3d02f41dfe8813b8b77a6f0d42285cb495
keziah55 <19816994+keziah55@users.noreply.github.com>
2021-04-30 20:08:32 +0100

    adding widgets init and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='CustomPyQtObjects',
+      version='0.1',
+      description='A repo of convenience classes for PyQt objects.',
+      author='Keziah Milligan',
+      py_modules=['widgets'],
+     )
