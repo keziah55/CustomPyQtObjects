02f6bd29fdbe0b1dcaa531d783f989f6f0e72663
keziah55 <19816994+keziah55@users.noreply.github.com>
2021-04-30 20:50:16 +0100

    moving files

---
+++ customQObjects/widgets/__init__.py
@@ -0,0 +1,4 @@
+from .timerdialog import TimerDialog 
+from .groupwidget import GroupWidget 
+
+__all__ = ["TimerDialog", "GroupWidget"]
---
+++ customQObjects/widgets/timerdialog.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import QDialog
+from PyQt5.QtCore import QTimer
+
+class TimerDialog(QDialog):
+    """ `QDialog <https://doc.qt.io/qt-5/qdialog.html>`_ that will timeout after a given number of milliseconds. 
+    
+        Parameters
+        ----------
+        timeout : int
+            Number of milliseconds for the dialog to be shown. Default is 3000.
+    """
+    
+    def __init__(self, timeout=3000):
+        super().__init__()
+        self.timer = QTimer()
+        self.timer.setInterval(timeout)
+        self.timer.setSingleShot(True)
+        self.timer.timeout.connect(self.accept)
+        
+    def exec_(self, *args, **kwargs):
+        """ Show the dialog and start the timer. """
+        self.timer.start()
+        super().exec_(*args, **kwargs)
+        
