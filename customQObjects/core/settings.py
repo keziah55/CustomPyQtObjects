ab7139a7e374cd5f44c50d33261734ad1184cc0f
keziah55 <19816994+keziah55@users.noreply.github.com>
2021-05-02 17:25:16 +0100

    typo

--- customQObjects/core/settings.py
+++ customQObjects/core/settings.py
@@ -2,8 +2,8 @@ from PyQt5.QtCore import QSettings
 
 class Settings(QSettings):
     """ QSettings subclass that provides :meth:`value` method that will cast
-        "true" and "false" to True or False, or will cast the returned value to t
-        he given type.
+        "true" and "false" to True or False, or will cast the returned value to
+        the given type.
     """
     def value(self, key, defaultValue=None, cast=None):
         """ Get value from settings. If value is "true" or "false", return True
