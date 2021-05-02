96a0c535fa2f813c23f87a004d2d9f1b7cf4f79a
keziah55 <19816994+keziah55@users.noreply.github.com>
2021-05-02 17:23:55 +0100

    add docstrings to Settings

--- customQObjects/core/settings.py
+++ customQObjects/core/settings.py
@@ -1,8 +1,24 @@
 from PyQt5.QtCore import QSettings
 
 class Settings(QSettings):
-    
+    """ QSettings subclass that provides :meth:`value` method that will cast
+        "true" and "false" to True or False, or will cast the returned value to t
+        he given type.
+    """
     def value(self, key, defaultValue=None, cast=None):
+        """ Get value from settings. If value is "true" or "false", return True
+            or False.
+            
+            Parameters
+            ----------
+            key : str
+                Setting to be returned
+            defaultValue : object, optional
+                If `key` is not in the settings, return `defaultValue`. Default
+                is None
+            cast : type, optional
+                If provided, cast the value to the given type
+        """
         v = super().value(key, defaultValue=defaultValue)
         if v == 'true':
             v = True
