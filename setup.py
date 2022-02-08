04839efaaeb442c5dfa9a93eae6fdc736f24f30e
keziah55 <19816994+keziah55@users.noreply.github.com>
2022-02-08 20:12:47 +0000

    updating setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 
 setup(name='CustomPyQtObjects',
       version='0.1',
-      description='A repo of convenience classes for PyQt objects.',
+      description='A repo of convenience classes for PyQt/PySide objects.',
       author='Keziah Milligan',
       packages = find_packages(),
-      install_requires = ["PyQt5"]
+      install_requires = ["QtPy"]
      )
