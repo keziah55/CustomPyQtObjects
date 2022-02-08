7dc0aeff88a910b78b34c8c31f60c59071eb560a
keziah55 <19816994+keziah55@users.noreply.github.com>
2022-02-08 20:29:31 +0000

    set packages list

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='CustomPyQtObjects',
       version='0.1',
       description='A repo of convenience classes for PyQt/PySide objects.',
       author='Keziah Milligan',
-      packages = find_packages(),
+      packages = ["customQObjects"], #find_packages(),
       install_requires = ["wheel", "QtPy"]
      )
