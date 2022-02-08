b92b65607351690b8c69406c30fc82f8215e3d90
keziah55 <19816994+keziah55@users.noreply.github.com>
2022-02-08 20:34:35 +0000

    adding init

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name='CustomPyQtObjects',
       version='0.1',
       description='A repo of convenience classes for PyQt/PySide objects.',
       author='Keziah Milligan',
-      packages = ["core", "widgets"], #find_packages(),
+      packages = find_packages(),
       install_requires = ["wheel", "QtPy"]
      )
