52f5e45224b7a80ecdc233299ef26c526c8f8f74
keziah55 <19816994+keziah55@users.noreply.github.com>
2022-02-16 21:17:13 +0000

    adding version

--- setup.py
+++ setup.py
@@ -2,8 +2,19 @@
 
 from setuptools import setup, find_packages
 
+import re
+VERSIONFILE="customQObjects/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+    
+
 setup(name='CustomPyQtObjects',
-      version='0.1',
+      version=verstr,
       description='A repo of convenience classes for PyQt/PySide objects.',
       author='Keziah Milligan',
       packages = find_packages(),
