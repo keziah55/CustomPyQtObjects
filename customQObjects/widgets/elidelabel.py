99c42166fd47a00dc1c5b8b50106a40babb870a0
keziah55 <19816994+keziah55@users.noreply.github.com>
2022-07-20 15:23:18 +0100

    ElideMixin widthAdjust before super.init

--- customQObjects/widgets/elidelabel.py
+++ customQObjects/widgets/elidelabel.py
@@ -8,9 +8,9 @@ class ElideMixin(object):
     
     def __init__(self, *args, elideMode='middle', widthAdjust=0, **kwargs):
         self._fullText = ""
+        self.widthAdjust = widthAdjust
         super().__init__(*args, **kwargs)
         self.elideMode = elideMode
-        self.widthAdjust = widthAdjust
         
     @property
     def fullText(self):
