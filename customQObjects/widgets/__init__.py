027e84788ffb69333c07597a9aadb7dc87ddeb22
keziah55 <19816994+keziah55@users.noreply.github.com>
2024-02-20 20:42:14 +0000

    adding ListSelector

--- customQObjects/widgets/__init__.py
+++ customQObjects/widgets/__init__.py
@@ -1,7 +1,8 @@
 from .elidemixin import ElideMixin, ElideLabel
 from .simplemixins import ClickMixin
 from .combobox import ComboBox
-from .groupbox import GroupBox 
+from .groupbox import GroupBox
+from .listselector import ListSelector 
 from .timerdialog import TimerDialog 
 from .spinbox import SpinBox, DoubleSpinBox
 from .stackedwidget import StackedWidget
@@ -10,4 +11,4 @@ from .tablewidget import TableWidget
 
 __all__ = ["TimerDialog", "GroupBox", "ComboBox", "ElideMixin", "ElideLabel",
            "ClickMixin",  "Splitter", "HSplitter", "VSplitter", "StackedWidget",
-           "SpinBox", "DoubleSpinBox", "TableWidget"]
+           "SpinBox", "DoubleSpinBox", "TableWidget", "ListSelector"]
