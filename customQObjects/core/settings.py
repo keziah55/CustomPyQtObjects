37a4a1ba6c934f1ea818a77acd0ba1e1a3a73f08
keziah55 <19816994+keziah55@users.noreply.github.com>
2025-01-30 21:51:17 +0000

    try-except settings value cast

--- customQObjects/core/settings.py
+++ customQObjects/core/settings.py
@@ -1,16 +1,18 @@
 from qtpy.QtCore import QSettings
 
+
 class Settings(QSettings):
-    """ 
-    [QSettings](https://doc.qt.io/qt-6/qsettings.html) subclass that provides 
+    """
+    [QSettings](https://doc.qt.io/qt-6/qsettings.html) subclass that provides
     [value][customQObjects.core.Settings.value] method that will cast
     "true" and "false" to True or False, or will cast the returned value to
     the given type.
     """
+
     def value(self, key, defaultValue=None, cast=None):
-        """ 
+        """
         Get value from settings. If value is "true" or "false", return True or False.
-            
+
         Parameters
         ----------
         key : str
@@ -19,13 +21,16 @@ class Settings(QSettings):
             If `key` is not in the settings, return `defaultValue`. Default
             is None
         cast : type, optional
-            If provided, cast the value to the given type
+            If provided (and if possible), cast the value to the given type.
         """
         v = super().value(key, defaultValue=defaultValue)
-        if v == 'true':
+        if v == "true":
             v = True
-        elif v == 'false':
+        elif v == "false":
             v = False
         elif cast is not None:
-            v = cast(v)
+            try:
+                v = cast(v)
+            except TypeError:
+                pass
         return v
